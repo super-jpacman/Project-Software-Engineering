71ddf3df14fb59868fee4e6bb5b46e1a1cfc099b
SaouZADEV <preechanon.c@kkumail.com>
2023-03-08 01:07:55 +0700

    Fix Bugs.

--- src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
+++ src/main/java/nl/tudelft/jpacman/MultiLevelLauncher.java
@@ -13,7 +13,7 @@ import java.util.List;
 
 public class MultiLevelLauncher extends Launcher {
 
-    private static final int NUMBER_OF_LEVELS = 1;
+    private static final int NUMBER_OF_LEVELS = 5;
 
     private MultiLevelGame multiGame;
     
@@ -37,7 +37,6 @@ public class MultiLevelLauncher extends Launcher {
 
 
         } finally {
-            System.err.println("989997987");
         }
 
 
