6f91b28dff070266b4996539c43f420c1267ac73
SaouZADEV <preechanon.c@kkumail.com>
2023-03-08 22:36:44 +0700

    add save score to JSON

--- src/main/java/nl/tudelft/jpacman/points/SaveScore.java
+++ src/main/java/nl/tudelft/jpacman/points/SaveScore.java
@@ -1,24 +1,40 @@
 package nl.tudelft.jpacman.points;
 
-import org.json.JSONObject;
+import org.json.simple.JSONObject;
+import org.json.simple.JSONArray;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
-import java.io.FileWriter;
+import java.io.*;
 
 public class SaveScore {
 
     public SaveScore(String Pname, double time, int point){
 
-        JSONObject save_ = new JSONObject();
-        save_.put("name", Pname);
-        save_.put("point", time);
-        save_.put("time", point);
+        JSONParser jsonParser = new JSONParser();
 
-        try (FileWriter file = new FileWriter("src/main/resources/score_board.json")) {
-            file.write(save_.toString());
+        try {
+
+            JSONArray jsonArray = (JSONArray) new JSONParser().parse(new FileReader("src/main/resources/score_board.json"));
+
+            JSONObject save_ = new JSONObject();
+            save_.put("name", Pname);
+            save_.put("point", point);
+            save_.put("time", time);
+
+            jsonArray.add(save_);
+
+            FileWriter file = new FileWriter("src/main/resources/score_board.json");
+            file.write(jsonArray.toJSONString());
             System.out.println("Successfully to Save JSON");
-        } catch(Exception e){
-            System.out.println(e);
+            file.flush();
+            file.close();
+
+        } catch (ParseException | IOException e) {
+            e.printStackTrace();
         }
+
+
     }
 
 }
