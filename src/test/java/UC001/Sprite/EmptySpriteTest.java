a4a60c81d5fbe6dc6ec15acdb50d98bac2c8d84c
Jitchaya <jitchaya.v@kkumail.com>
2023-03-10 23:02:59 +0700

    Edit EmptySpriteTest

--- src/test/java/UC001/Sprite/EmptySpriteTest.java
+++ src/test/java/UC001/Sprite/EmptySpriteTest.java
@@ -24,8 +24,8 @@ public class EmptySpriteTest {
         sprite = store.loadSprite("/TestSprite/64x64white.png");
     }
     @Test
-    @DisplayName("TC01: function getPelletSprite")
-    void testDraw() {
+    @DisplayName("TC01: function draw() in EmptySprite")
+    public void TC01() {
         EmptySprite empty = new EmptySprite();
         assertDoesNotThrow(() -> {
             empty.draw(null, 0, 0, 0, 0);
@@ -33,30 +33,31 @@ public class EmptySpriteTest {
     }
 
     @Test
-    void testSplit() {
-        // Create an empty sprite and call the split method with some
-        // arbitrary parameters. The result should be a new empty sprite.
+    @DisplayName("TC02: function split() in EmptySprite")
+    public void TC02() {
         EmptySprite empty = new EmptySprite();
         Sprite result = empty.split(0, 0, 0, 0);
         assertTrue(result instanceof EmptySprite);
     }
 
     @Test
-    void testGetWidth() {
+    @DisplayName("TC03: function getWidth() in EmptySprite")
+    public void TC03() {
         // Create an empty sprite and make sure the width is 0.
         EmptySprite empty = new EmptySprite();
         assertEquals(0, empty.getWidth());
     }
 
     @Test
-    void testGetHeight() {
+    @DisplayName("TC04: function getHeight() in EmptySprite")
+    public void TC04() {
         // Create an empty sprite and make sure the height is 0.
         EmptySprite empty = new EmptySprite();
         assertEquals(0, empty.getHeight());
     }
-    @DisplayName("TC08: function split instance of EmptySprite")
+    @DisplayName("TC05: function split instance of EmptySprite")
     @Test
-    public void TC08() throws InterruptedException {
+    public void TC05() throws InterruptedException {
         Sprite split = sprite.split(10, 10, 64, 10);
         assertThat(split).isInstanceOf(EmptySprite.class);
     }
