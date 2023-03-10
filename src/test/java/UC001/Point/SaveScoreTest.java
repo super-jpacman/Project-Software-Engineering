18ab80c080161b1765ee3dfb107f14686912e085
Jitchaya <jitchaya.v@kkumail.com>
2023-03-10 20:19:10 +0700

    Edit TestSaveScore

--- src/test/java/UC001/Point/SaveScoreTest.java
+++ src/test/java/UC001/Point/SaveScoreTest.java
@@ -13,7 +13,7 @@ public class SaveScoreTest {
     @DisplayName("TC01:Save name, score and time")
     @Test
     void TC01() throws IOException {
-        String name = "Test Player";
+        String name = "TestPlayer";
         int point = 100;
         double time = 50.0;
         SaveScore saveScore = new SaveScore(name, time, point);
@@ -33,6 +33,62 @@ public class SaveScoreTest {
     @DisplayName("TC02:Save name more 16, score and time")
     @Test
     void TC02() throws IOException {
+        String name = "TestPlayerTestPlus";
+        int point = 100;
+        double time = 50.0;
+        SaveScore saveScore = new SaveScore(name, time, point);
+        File file = new File("src/main/resources/score_board.json");
+        FileReader reader = new FileReader(file);
+        int c;
+        StringBuilder sb = new StringBuilder();
+        while ((c = reader.read()) != -1) {
+            sb.append((char) c);
+        }
+        reader.close();
+        String json = sb.toString();
+        Assertions.assertFalse(json.contains("\"name\":\"TestPlayerTestPlus\""));
+        Assertions.assertTrue(json.contains("\"point\":100"));
+        Assertions.assertTrue(json.contains("\"time\":50.0"));
+    }
+    @DisplayName("TC03:Save name more 16, score and time")
+    @Test
+    void TC03() throws IOException {
+        String name = "TestPlayer";
+        int point = -100;
+        double time = 50.0;
+        SaveScore saveScore = new SaveScore(name, time, point);
+        File file = new File("src/main/resources/score_board.json");
+        FileReader reader = new FileReader(file);
+        int c;
+        StringBuilder sb = new StringBuilder();
+        while ((c = reader.read()) != -1) {
+            sb.append((char) c);
+        }
+        reader.close();
+        String json = sb.toString();
+        Assertions.assertFalse(json.contains("\"point\":-100"));
+    }
+    @DisplayName("TC04: Save name, score and time")
+    @Test
+    void TC04() throws IOException {
+        String name = "TestPlayerTestPlus";
+        int point = 100;
+        double time = 50.0;
+        SaveScore saveScore = new SaveScore(name, time, point);
+        File file = new File("src/main/resources/score_board.json");
+        FileReader reader = new FileReader(file);
+        int c;
+        StringBuilder sb = new StringBuilder();
+        while ((c = reader.read()) != -1) {
+            sb.append((char) c);
+        }
+        reader.close();
+        String json = sb.toString();
+        Assertions.assertTrue(json.contains("\"point\":100"));
+    }
+    @DisplayName("TC05:Save name more 16, score and time")
+    @Test
+    void TC05() throws IOException {
         String name = "TestPlayerTestPlus";
         int point = 100;
         double time = 50.0;
